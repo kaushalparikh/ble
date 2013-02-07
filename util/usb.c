78b0590735a4e4df7e87400021c951ee37accc63
Kaushal Parikh <kaushalparikh@gmail.com>
2013-02-07 17:22:07 +0530

    USB utility functions for connect/disconnect and finding hub port

--- util/usb.c
+++ util/usb.c
@@ -6,214 +6,215 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#if 0
+#include <usb.h>
+#include <sys/ioctl.h>
+#include <linux/usbdevice_fs.h>
 
 #include <libudev.h>
-#include <libusb.h>
 
-int libudev_main (char * vendor_id, char * product_id)
+
+int usb_find (char * vendor_id, char * product_id)
 {
-	struct udev *udev;
-	struct udev_enumerate *enumerate;
-	struct udev_list_entry *devices, *dev_list_entry;
+  struct udev *udev;
+  struct udev_enumerate *udev_enumerate;
+  struct udev_list_entry *dev_list, *dev_list_entry;
   
-  libusb_device **devs;
-  int r;
-  ssize_t cnt;
+  /* Create the udev object */
+  udev = udev_new ();
+  if (udev == NULL)
+  {
+    printf("Can't create udev\n");
+    return 1;
+  }
+  
+  /* Create a list of the devices in the 'tty' subsystem */
+  udev_enumerate = udev_enumerate_new (udev);
+  udev_enumerate_add_match_subsystem (udev_enumerate, "tty");
+  udev_enumerate_scan_devices (udev_enumerate);
+  dev_list = udev_enumerate_get_list_entry (udev_enumerate);
 
-  int fd = -1;
-	
-	/* Create the udev object */
-	udev = udev_new();
-	if (!udev) {
-		printf("Can't create udev\n");
-		return 1;
-	}
-	
-  r = libusb_init (NULL);
-  if (r < 0)
-    return r;
+  /* For each item enumerated, print out its information.
+     udev_list_entry_foreach is a macro which expands to
+     a loop. The loop will be executed for each member in
+     devices, setting dev_list_entry to a list entry
+     which contains the device's path in /sys */
+  udev_list_entry_foreach (dev_list_entry, dev_list) {
+    struct udev_device *dev, *dev_par;
+    const char *dev_path;
+    
+    /* Get the filename of the /sys entry for the device
+       and create a udev_device object (dev) representing it
+       usb_device_get_devnode() returns the path to the device node
+       itself in /dev. */
+    dev_path = udev_list_entry_get_name (dev_list_entry);
+    dev = udev_device_new_from_syspath (udev, dev_path);
 
-	/* Create a list of the devices in the 'hidraw' subsystem. */
-	enumerate = udev_enumerate_new(udev);
-	
-  /* udev_enumerate_add_match_subsystem(enumerate, "tty");
-	udev_enumerate_scan_devices(enumerate); */
-  udev_enumerate_add_match_subsystem(enumerate, "tty");
-  /* udev_enumerate_add_match_property(enumerate, "ID_VENDOR_ID", "2458");
-  udev_enumerate_add_match_property(enumerate, "ID_MODEL_ID", "0001"); */
-	udev_enumerate_scan_devices(enumerate);
-	
-  cnt = libusb_get_device_list(NULL, &devs);
-  if (cnt < 0)
-    return (int) cnt;
-
-  devices = udev_enumerate_get_list_entry(enumerate);
-	/* For each item enumerated, print out its information.
-	   udev_list_entry_foreach is a macro which expands to
-	   a loop. The loop will be executed for each member in
-	   devices, setting dev_list_entry to a list entry
-	   which contains the device's path in /sys. */
-	udev_list_entry_foreach(dev_list_entry, devices) {
-	  struct udev_device *dev, *dev_par;
-		const char *path;
-		
-		/* Get the filename of the /sys entry for the device
-		   and create a udev_device object (dev) representing it
-		   usb_device_get_devnode() returns the path to the device node
-		   itself in /dev. */
-		path = udev_list_entry_get_name(dev_list_entry);
-		dev = udev_device_new_from_syspath(udev, path);
-
-		/* The device pointed to by dev contains information about
-		   the hidraw device. In order to get information about the
-		   USB device, get the parent device with the
-		   subsystem/devtype pair of "usb"/"usb_device". This will
-		   be several levels up the tree, but the function will find
-		   it.*/
-		dev_par = udev_device_get_parent_with_subsystem_devtype(
-		            dev,
-		            "usb",
-		            "usb_device");
-		if (dev_par != NULL)
+    /* The device pointed to by dev contains information about
+       the hidraw device. In order to get information about the
+       USB device, get the parent device with the
+       subsystem/devtype pair of "usb"/"usb_device". This will
+       be several levels up the tree, but the function will find
+       it */
+    dev_par = udev_device_get_parent_with_subsystem_devtype (dev,
+                                                             "usb",
+                                                             "usb_device");
+    if (dev_par != NULL)
     {
-		  /* From here, we can call get_sysattr_value() for each file
-		     in the device's /sys entry. The strings passed into these
-		     functions (idProduct, idVendor, serial, etc.) correspond
-		     directly to the files in the directory which represents
-		     the USB device. Note that USB strings are Unicode, UCS2
-		     encoded, but the strings returned from
-		     udev_device_get_sysattr_value() are UTF-8 encoded. */
-      if (((strcmp (vendor_id, (udev_device_get_sysattr_value(dev_par, "idVendor")))) == 0) &&
-          ((strcmp (product_id, (udev_device_get_sysattr_value(dev_par, "idProduct")))) == 0))
+      /* From here, we can call get_sysattr_value() for each file
+         in the device's /sys entry. The strings passed into these
+         functions (idProduct, idVendor, serial, etc.) correspond
+         directly to the files in the directory which represents
+         the USB device. Note that USB strings are Unicode, UCS2
+         encoded, but the strings returned from
+         udev_device_get_sysattr_value() are UTF-8 encoded */
+      if (((strcmp (vendor_id, (udev_device_get_sysattr_value (dev_par, "idVendor")))) == 0) &&
+          ((strcmp (product_id, (udev_device_get_sysattr_value (dev_par, "idProduct")))) == 0))
       {
-        libusb_device *dev1;
-        int i = 0;
+        printf ("Device VID/PID %s/%s\n", udev_device_get_sysattr_value (dev_par, "idVendor"),
+                                          udev_device_get_sysattr_value (dev_par, "idProduct"));
+        printf ("       Type %s\n", udev_device_get_devtype (dev_par));
+        printf ("       Node %s\n", udev_device_get_devnode (dev_par));
+        printf ("       Path %s\n", udev_device_get_syspath (dev_par));
+        printf ("       Interface Path %s\n", udev_device_get_devpath (dev));
 
-        while ((dev1 = devs[i++]) != NULL)
+        while ((dev_par = udev_device_get_parent (dev_par)) != NULL)
         {
-          struct libusb_device_descriptor desc;
-          int r = libusb_get_device_descriptor (dev1, &desc);
-          if (r < 0)
+          const char *dev_class = udev_device_get_sysattr_value (dev_par, "bDeviceClass");
+
+          if (dev_class == NULL)
           {
-            printf("Failed to get device descriptor");
             continue;
           }
-
-          if ( ((strtol (vendor_id, NULL, 16)) == desc.idVendor) &&
-               ((strtol (product_id, NULL, 16)) == desc.idProduct) &&
-               ((atoi (udev_device_get_sysattr_value (dev_par, "busnum"))) == libusb_get_bus_number (dev1)) &&
-               ((atoi (udev_device_get_sysattr_value (dev_par, "devnum"))) == libusb_get_device_address (dev1)) )
-
+            
+          if (atoi (dev_class) == USB_CLASS_HUB)
           {
-            printf ("%04x:%04x (bus %d, device %d)\n",
-                     desc.idVendor, desc.idProduct,
-                     libusb_get_bus_number(dev1), libusb_get_device_address (dev1));
+            struct usbdevfs_ioctl ioctl_param;
+            struct usbdevfs_hub_portinfo hub_portinfo;
+            int fd;
+            const char *dev_node = udev_device_get_devnode (dev_par);
 
-            printf ("Opening %s\n", udev_device_get_devnode (dev));
-            fd = open ((udev_device_get_devnode (dev)), (O_RDWR | O_NOCTTY /*| O_NDELAY*/));
-            if (fd >= 0)
+            printf ("Hub VID/PID %s/%s\n", udev_device_get_sysattr_value (dev_par, "idVendor"),
+                                              udev_device_get_sysattr_value (dev_par, "idProduct"));
+            printf ("    Type %s\n", udev_device_get_devtype (dev_par));
+            printf ("    Node %s\n", dev_node);
+            printf ("    Path %s\n", udev_device_get_syspath (dev_par));
+
+            ioctl_param.ifno       = 0;
+            ioctl_param.ioctl_code = USBDEVFS_HUB_PORTINFO;
+            ioctl_param.data       = &hub_portinfo;
+  
+            fd = open (dev_node, O_RDWR);
+            if (fd > 0)
             {
-              if ((lockf (fd, F_TLOCK, 0)) < 0)
+              if ((ioctl (fd, USBDEVFS_IOCTL, &ioctl_param)) >= 0)
               {
-                printf ("Device in use, %s\n", strerror (errno));
-                close (fd);
-                fd = -1;
+                int i;
+                printf ("    Number of Ports %d\n", hub_portinfo.nports);
+                for (i = 0; i < hub_portinfo.nports; i++)
+                {
+                  printf ("    Port %d Device Number %d\n", (i+1), hub_portinfo.port[i]);
+                }
+              }
+              else
+              {
+                printf ("    Unable to read hub port info\n");
               }
             }
             else
             {
-              printf ("Unable to open\n");
+              printf ("    Unable to open hub device node\n");
             }
 
             break;
+            
           }
         }
+
       }
-		  
-      udev_device_unref(dev_par);
-		}
-    else
-    {
-      udev_device_unref(dev);
     }
 
-    if (fd > 0)
-    {
-      break;
-    }
-	}
-
-  if (fd > 0)
-  {
-    close (fd);
+    udev_device_unref (dev);
   }
+  
+  /* Free the enumerator object */
+  udev_enumerate_unref (udev_enumerate);
+  udev_unref (udev);
 
-  libusb_free_device_list (devs, 1);
-  libusb_exit (NULL);
-
-	/* Free the enumerator object */
-	udev_enumerate_unref(enumerate);
-
-	udev_unref(udev);
-
-	return 0;       
+  return 0;       
 }
 
+int usb_disconnect (char *dev_node,
+                    int   interface)
+{
+  int fd, status;
+
+  status = 0;
+  fd = open (dev_node, O_RDWR);
+  if (fd > 0)
+  {
+    struct usbdevfs_ioctl param;
+
+    memset (&param, 0, sizeof (param));
+    
+    param.ifno       = interface;
+    param.ioctl_code = USBDEVFS_DISCONNECT;
+    param.data       = NULL;
+
+    status = ioctl (fd, USBDEVFS_IOCTL, &param);
+    if (status < 0)
+    {
+      printf ("Unable to disconnect device node %s interface %d\n", dev_node, interface);
+    }
+    close (fd);
+  }
+  else
+  {
+    status = -1;
+    printf ("Unable to open device node %s\n", dev_node);
+  }
+
+  return status;
+}
+
+int usb_connect (char *dev_node,
+                 int   interface)
+{
+  int fd, status;
+
+  status = 0;
+  fd = open (dev_node, O_RDWR);
+  if (fd > 0)
+  {
+    struct usbdevfs_ioctl param;
+
+    memset (&param, 0, sizeof (param));
+    
+    param.ifno       = interface;
+    param.ioctl_code = USBDEVFS_CONNECT;
+    param.data       = NULL;
+
+    status = ioctl (fd, USBDEVFS_IOCTL, &param);
+    if (status < 0)
+    {
+      printf ("Unable to connect device node %s interface %d\n", dev_node, interface);
+    }
+    close (fd);
+  }
+  else
+  {
+    status = -1;
+    printf ("Unable to open device node %s\n", dev_node);
+  }
+  
+  return status;
+}
 
 int main (void)
 {
   printf ("\n");
-  libudev_main ("2458", "0001");
+  usb_find ("2458", "0001");
   printf ("\n");
 
   return 0;
 }
-
-#else
-
-#include <sys/ioctl.h>
-#include <linux/usbdevice_fs.h>
-
-int usb_disconnect (char * dev_node,
-                    int    interface)
-{
-  struct usbdevfs_ioctl param;
-  memset (&param, 0, sizeof (param));
-  
-  param.ifno       = interface;
-  param.ioctl_code = USBDEVFS_DISCONNECT;
-  param.data       = NULL;
-  
-  return ioctl (fd, USBDEVFS_IOCTL, &param);
-}
-
-int usb_connect (char * dev_node,
-                 int    interface)
-{
-  struct usbdevfs_ioctl param;
-  memset (&param, 0, sizeof (param));
-  
-  param.ifno       = interface;
-  param.ioctl_code = USBDEVFS_CONNECT;
-  param.data       = NULL;
-  
-  return ioctl (fd, USBDEVFS_IOCTL, &param);
-}
-
-int main (int argc, char **argv)
-
-{
-  struct usbdevfs_ioctl param;
-	const char *filename;
-	int fd;
-
-	filename = argv[1];
-	fd = open (filename, O_WRONLY);
-  close (fd);
-
-	return 0;
-}
-
-#endif
 
